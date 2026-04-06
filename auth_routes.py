ee198bf76768553e09b4724eb334264a10c138f0
ezeepires <ezequiel480235@gmail.com>
2026-04-05 22:59:34 -0300

    feat(auth): implementa rotas de cadastro de usuario com hash de senha e schemas

--- auth_routes.py
+++ auth_routes.py
@@ -1,13 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import Usuario
+from dependencies import pegar_sessao
+from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 # dominio/auth/ - para manter ordem e organizacao
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
     """
     Essa e a rota padrao de autenticacao do nosso sistema
     """ 
     # Isso acima e chamado de comentarios para APIs
     return {"mensagem": "Voce acessou a rota padrao de autenticacao", "autenticado": False}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
+    if usuario:
+        # Ja existe um usuario com esse email
+        raise HTTPException(status_code=400, detail="email do usuario ja cadastrado")
+    else:
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
+        session.add(novo_usuario)
+        session.commit()
+        return {"mensagem": f"usuario cadastrado com sucesso {usuario_schema.email}"}
 
