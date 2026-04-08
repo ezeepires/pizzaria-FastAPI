d4c20123464774f249d4819bbfc7e8164b46058a
ezeepires <ezequiel480235@gmail.com>
2026-04-08 00:06:36 -0300

    Implementação de JWT tokens na autenticação

--- auth_routes.py
+++ auth_routes.py
@@ -1,16 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 # dominio/auth/ - para manter ordem e organizacao
 
-def criar_token(id):
-    token = f"sbfbsauiehbdlks{id}"
-    return token
+def criar_token(id_usuario, duracao_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_codificado
+
+def verificar_token(token, session: Session = Depends(pegar_sessao)):
+    # verificar se o token e valido
+    # extrair o ID do usuario do token
+    usuario = session.query(Usuario).filter(Usuario.id==1).first()
+    return usuario
+
+def autenticar_usuario(email, senha, session):
+    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    if not usuario:
+        return False
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+    return usuario
 
 @auth_router.get("/")
 async def home():
@@ -36,15 +54,23 @@ async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(
 # login -> email e senha -> Token JWT (Json Web Token) fdgsdfdsf544dsdsas545dfsdf54f44f5g5
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email==login_schema.email).first()
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
     if not usuario:
-        raise HTTPException(status_code=400, detail="Usuario nao encontrado")
+        raise HTTPException(status_code=400, detail="Usuario nao encontrado ou credenciais invalidas!")
     else:
         access_token = criar_token(usuario.id)
+        refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
         return {
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "token_type": "Bearer"
+            }
+
+@auth_router.get("/refresh")
+async def use_refresh_token(token):
+    usuario = verificar_token(token)
+    access_token = criar_token(usuario.id)
+    return {
             "access_token": access_token,
             "token_type": "Bearer"
             }
-        # JWT Bearner
-
-        # headers = {"Access-Token": "Bearer token"}
